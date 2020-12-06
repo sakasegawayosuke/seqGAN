cca847a5ce30e7406b1c6ed12bd2edb9f4b49241
sakasegawayosuke <60595083+sakasegawayosuke@users.noreply.github.com>
2020-12-06 16:25:51 +0900

    Update sequence_gan.py

--- sequence_gan.py
+++ sequence_gan.py
@@ -65,7 +65,7 @@ def main():
     tf.random.set_seed(SEED)
     assert START_TOKEN == 0
 
-    vocab_size = 1197
+    vocab_size = 1198
 
     physical_devices = tf.config.experimental.list_physical_devices("GPU")
     if len(physical_devices) > 0:
